c7cfb880feb2f2ef68930f966e8abc251116548c
whitwhoa <jason@youtorque.com>
2023-07-28 10:11:24 -0500

    -

---
+++ src/Dren/Router.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Dren;
+
+use Dren\Exceptions\NotFound;
+
+class Router
+{
+    private static $instance = null;
+
+    private static function init()
+    {
+        if (self::$instance == null)
+            self::$instance = new Router();
+    }
+
+    private array $routes;
+
+    private ?int $activeRoute;
+
+    private function __construct()
+    {
+        $this->routes = [];
+    }
+
+    public static function web() : Router
+    {
+        self::init();
+
+        $r = new Route();
+        $r->setRouteType('web');
+
+        self::$instance->routes[] = $r;
+
+        return self::$instance;
+    }
+
+    public static function mobile() : Router
+    {
+        self::init();
+
+        $r = new Route();
+        $r->setRouteType('mobile');
+
+        self::$instance->routes[] = $r;
+
+        return self::$instance;
+    }
+
+    public static function api() : Router
+    {
+        self::init();
+
+        $r = new Route();
+        $r->setRouteType('api');
+
+        self::$instance->routes[] = $r;
+
+        return self::$instance;
+    }
+
+    public static function setActiveRoute(string $requestedUri): void
+    {
+        self::init();
+
+        $i = 0;
+        foreach(self::$instance->routes as $r)
+        {
+           if(preg_match($r->getUriRegex(), $requestedUri))
+           {
+               $r->setUriParameters();
+               self::$instance->activeRoute = $i;
+               return;
+           }
+
+            $i++;
+        }
+
+        throw new NotFound('Route not found');
+    }
+
+    public static function getActiveRoute(): Route
+    {
+        return self::$instance->routes[self::$instance->activeRoute];
+    }
+
+    public function get(string $uriString) : Router
+    {
+        $route = end($this->routes);
+        $route->setRequestMethod('get');
+        $route->setUri($uriString);
+
+        return $this;
+    }
+
+    public function controller(string $controllerString) : Router
+    {
+        $route = end($this->routes);
+        $route->setController($controllerString);
+
+        return $this;
+    }
+
+    public function method(string $methodString) : Router
+    {
+        $route = end($this->routes);
+        $route->setMethod($methodString);
+
+        return $this;
+    }
+
+    public function middleware(array $middleware) : Router
+    {
+        $route = end($this->routes);
+        $route->setMiddleware($middleware);
+
+        return $this;
+    }
+
+    public function formDataValidator(string $formDataValidatorString) : Router
+    {
+        $route = end($this->routes);
+        $route->setFormDataValidator($formDataValidatorString);
+
+        return $this;
+    }
+
+}
