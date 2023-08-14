441d83132ba4030501c2cbe66b09ac8c06abcd91
whitwhoa <jason@youtorque.com>
2023-08-14 10:21:30 -0500

    FileLockingDataStore first draft implemented, time to refactor SessionManager to use it

--- src/Dren/FileLockingDataStore.php
+++ src/Dren/FileLockingDataStore.php
@@ -2,23 +2,139 @@
 
 namespace Dren;
 
+use Exception;
+
 class FileLockingDataStore implements LockingDataStore
 {
+    private ?string $lockId;
     private mixed $fileResource;
 
     public function __construct()
     {
+        $this->lockId = null;
         $this->fileResource = null;
+    }
+
+    public function openLock(string $lockId): bool
+    {
+        // Open the file for reading and writing. If it doesn't exist, create it.
+        $this->fileResource = fopen($lockId, 'c+');
+
+        // Check if there was a problem opening/creating the file
+        if($this->fileResource === false)
+            return false;
+
+        // Now, if file was opened successfully, try to get a lock.
+        if (flock($this->fileResource, LOCK_EX) === false) // Attempt to acquire an exclusive lock, block and wait if one cannot be acquired
+            return false;
+
+        $this->lockId = $lockId;
+
+        return true;
     }
 
     public function openLockIfExists(string $lockId): bool
     {
-        
+        // Open the file for reading and writing. Suppress the error here as we don't care about it since we're using
+        // the error logic to determine if the file is present or not, and if not we're returning false
+        $this->fileResource = @fopen($lockId, 'r+');
+
+        if($this->fileResource === false)
+            return false;
+
+        // Now, if file was opened successfully, try to get a lock.
+        if (flock($this->fileResource, LOCK_EX) === false) // Attempt to acquire an exclusive lock, block and wait if one cannot be acquired
+            return false;
+
+        $this->lockId = $lockId;
+
+        return true;
     }
 
-
-    public function openLock(string $lockId): void
+    /**
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function closeLock(): void
     {
-        // TODO: Implement this
+        if (!is_resource($this->fileResource))
+            throw new Exception("Attempting to close file resource which is not valid.");
+
+        $unlockSuccess = flock($this->fileResource, LOCK_UN);
+        if (!$unlockSuccess)
+            throw new Exception("Failed to unlock file.");
+
+        $closeSuccess = fclose($this->fileResource);
+        if (!$closeSuccess)
+            throw new Exception("Failed to close file.");
+
+        $this->fileResource = null;
     }
+
+    /**
+     * @throws Exception
+     */
+    public function getContents(): string
+    {
+        if(!$this->lockId)
+            throw new Exception('Attempting to getContents of FileLockingDataStore which has no lock id');
+
+        if(!$this->fileResource)
+            throw new Exception('Attempting to getContents of FileLockingDataStore which has no File Resource');
+
+        clearstatcache(true, $this->lockId); // Clear stat cache for the file
+        $contents = fread($this->fileResource, filesize($this->lockId)); // Read the entire file
+
+        if(!$contents)
+            throw new Exception('Failed to retrieve file contents');
+
+        return $contents;
+    }
+
+    /**
+     *
+     * @param string $dataToWrite
+     * @return void
+     * @throws Exception
+     */
+    public function overwriteContents(string $dataToWrite): void
+    {
+        if (!is_resource($this->fileResource))
+            throw new Exception("Provided file resource is not valid.");
+
+        $truncateSuccess = ftruncate($this->fileResource, 0);
+        if (!$truncateSuccess)
+            throw new Exception("Failed to truncate file.");
+
+        $rewindSuccess = rewind($this->fileResource);
+        if (!$rewindSuccess)
+            throw new Exception("Failed to rewind file pointer.");
+
+        $bytesWritten = fwrite($this->fileResource, $dataToWrite);
+        if ($bytesWritten === false)
+            throw new Exception("Failed to write to file.");
+    }
+
+    /**
+     *
+     * @param string $dataToWrite
+     * @return void
+     * @throws Exception
+     */
+    public function appendContents(string $dataToWrite): void
+    {
+        if (!is_resource($this->fileResource))
+            throw new Exception("Provided file resource is not valid.");
+
+        // Seek to the end of the file
+        $seekSuccess = fseek($this->fileResource, 0, SEEK_END);
+        if ($seekSuccess === -1)
+            throw new Exception("Failed to seek to the end of the file.");
+
+        $bytesWritten = fwrite($this->fileResource, $dataToWrite);
+        if ($bytesWritten === false)
+            throw new Exception("Failed to write to file.");
+    }
+
 }
--- src/Dren/LockingDataStore.php
+++ src/Dren/LockingDataStore.php
@@ -8,9 +8,10 @@ interface LockingDataStore
      * Open a lock. Create the data store element on which to lock if it does not exist
      *
      * @param string $lockId
-     * @return void
+     * @return bool
      */
-    public function openLock(string $lockId) : void;
+    public function openLock(string $lockId) : bool;
+
 
     /**
      * Open a lock if it's data store already exists and return true. Return false if it does not
@@ -19,4 +20,38 @@ interface LockingDataStore
      * @return bool
      */
     public function openLockIfExists(string $lockId) : bool;
+
+
+    /**
+     * Close the lock
+     *
+     * @return void
+     */
+    public function closeLock() : void;
+
+
+    /**
+     * Read contents of locked data store resource and return as string
+     *
+     * @return mixed
+     */
+    public function getContents() : string;
+
+
+    /**
+     * Overwrite the entire contents of the data store with the contents of the provided parameter
+     *
+     * @param string $dataToWrite
+     * @return void
+     */
+    public function overwriteContents(string $dataToWrite) : void;
+
+
+    /**
+     * Append to the existing contents of the data store, the contents of the provided parameter
+     *
+     * @param string $dataToWrite
+     * @return void
+     */
+    public function appendContents(string $dataToWrite) : void;
 }
